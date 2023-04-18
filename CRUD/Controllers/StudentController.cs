c2569bc0d7c97872d6cf8864a662e513337dae1c
nourahamed111 <nourahamed111@gmail.com>
2023-04-18 12:11:19 +0200

    backend

---
+++ CRUD/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentContext _studentContext;
+        
+        //constructor to initiate student contex
+        public StudentController(StudentContext studentContext)
+        {
+            _studentContext = studentContext;
+        }
+        [HttpGet]
+        [Route("GetStudent")]
+        public async Task<IEnumerable<Student>>GetStudents()
+        {
+            return await _studentContext.Students.ToListAsync();
+        }
+
+        [HttpPost]
+        [Route("AddStudent")]
+        public async Task<Student> AddStudent( Student objStudent)
+        {
+            _studentContext.Students.Add(objStudent);
+            await _studentContext.SaveChangesAsync();
+            return objStudent;
+        }
+
+        [HttpPatch]
+        [Route("UpdateStudent/{id}")]
+        public async Task<Student> UpdateStudent(Student objStudent)
+        {
+            _studentContext.Students.Entry(objStudent).State= EntityState.Modified;
+            await _studentContext.SaveChangesAsync();
+            return objStudent;
+        }
+ 
+        [HttpDelete]
+        [Route("DeleteStudent/{id}")]
+        public string DeleteStudent(int id)
+        {
+            string a  ;
+            var student = _studentContext.Students.Find(id);
+            if (student != null)
+            {
+                a = "Student Deleted";
+
+                _studentContext.Students.Entry(student).State = EntityState.Deleted;
+                 _studentContext.SaveChanges();
+            }
+            else
+            {
+                a = "Sory we can not find this student";
+            }
+         
+            return  a;
+        }
+
+
+    }
+}
---
+++ CRUD/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Courses { get; set; }
+
+
+    }
+}
